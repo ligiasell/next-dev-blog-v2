6f1714b562a355a0b0dc24bce92539e30679e939
Lígia Sell <ligia.sell@hotmail.com>
2023-02-03 16:35:30 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // To add customized dark mode
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-dark": "#1f1f1f",
+        primary: "#ffffff",
+        highlight: {
+          dark: "#FFFFFF",
+          light: "#1f1f1f",
+        },
+        secondary: {
+          dark: "#707070",
+          light: "#e6e6e6",
+        },
+        action: "#3B82F6",
+      },
+      transitionProperty: {
+        width: "width",
+      },
+    },
+  },
+  plugins: [],
+};
